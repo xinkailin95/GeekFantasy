fa20153371a2d27e48adb3469a4436bc48158e35
xinkai <xinkailin1995@gmail.com>
2018-02-21 12:11:00 -0500

    enemy movement

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+
+	private Animator anim;
+	private Vector3 prevPosition;
+	private Vector3 newPosition;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+		
+	// Update is called once per frame
+	void Update ()
+	{
+		newPosition = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.A))	// move left
+			newPosition.x = -1f;
+		if (Input.GetKey (KeyCode.D))	// move right
+			newPosition.x = 1f;	
+		if (Input.GetKey (KeyCode.W))	// move up
+			newPosition.y = 1f;
+		if (Input.GetKey (KeyCode.S))	// move down
+			newPosition.y = -1f;
+		// no diagonal movement
+		if (Input.GetKey (KeyCode.A) && Input.GetKey (KeyCode.W) || (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D)) || (Input.GetKey (KeyCode.D) && Input.GetKey (KeyCode.S)) || (Input.GetKey (KeyCode.S) && Input.GetKey (KeyCode.A))) {
+			anim.SetBool ("iswalking", false);
+			newPosition = prevPosition;
+		}
+
+		transform.position += newPosition * speed * Time.deltaTime;
+
+		if (newPosition.sqrMagnitude > 0) {
+			anim.SetBool ("iswalking", true);
+			prevPosition = newPosition;
+		} else {
+			anim.SetBool ("iswalking", false);
+			newPosition = prevPosition;
+		}
+
+		anim.SetFloat ("input_x", newPosition.x);
+		anim.SetFloat ("input_y", newPosition.y);
+
+	}
+
+}
