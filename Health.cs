c7f4b78bb6a98b6c092e5cbb5c096ac0cf1f4c5b
xingtongli <32304597+xingtongli@users.noreply.github.com>
2018-02-21 16:26:07 -0500

    main map

---
+++ Health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public int characterMaxHP;
+	public int characterCurrentHP;
+
+	// Use this for initialization
+	void Start () {
+		characterCurrentHP = characterMaxHP;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (characterCurrentHP <= 0) {
+			gameObject.SetActive (false);
+			
+		}
+	}
+	public void Hurt(int damage)
+	{
+		characterCurrentHP = characterCurrentHP - damage;
+	}
+	public void setMaxHP()
+	{
+		characterCurrentHP = characterMaxHP;
+	}
+}
---
+++ Hurt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hurt : MonoBehaviour {
+
+	public int damage;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void theCollision(Collision other)
+	{
+		if (other.gameObject.name == "Player") 
+		{
+			other.gameObject.GetComponent<Health> ().Hurt (damage);
+		}
+	}
+
+}
