fab93a0e8e97f6bede24b7e9e9b331c6b43b8e78
Hui Gao <huigao@HuideMacBook-Pro.local>
2018-03-14 21:47:34 -0400

    add level-up system and enemy die

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float moveSpeed = 9;
+	public Vector3 bulletEulerAngles;
+
+
+	private SpriteRenderer sr;
+	public Sprite[] playerSprite;// up right left down
+
+
+	private void Awake(){
+		sr = GetComponent<SpriteRenderer> ();
+	}
+ 	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Attack ();
+	}
+	public void FixedUpdate(){
+		float h = Input.GetAxisRaw ("Horizontal");
+		transform.Translate (Vector3.right * h * moveSpeed * Time.deltaTime, Space.World);
+		if (h < 0) {
+			sr.sprite = playerSprite [2];
+			bulletEulerAngles = new Vector3 (0, 0, 90);
+
+		} 
+		else if (h > 0) {
+			sr.sprite = playerSprite [1];
+			bulletEulerAngles = new Vector3 (0, 0, -90);	
+		}
+		float v = Input.GetAxisRaw ("Vertical");
+		transform.Translate (Vector3.up * v * moveSpeed * Time.deltaTime, Space.World);
+
+		if (v < 0) {
+			sr.sprite = playerSprite [3];
+			bulletEulerAngles = new Vector3 (0, 0, -180);
+		}
+		else if (v > 0) {
+			sr.sprite = playerSprite [0];
+			bulletEulerAngles = new Vector3 (0, 0, 0);
+		}
+	}
+	private void Attack(){
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate (bulletPrefab, transform.position, Quaternion.Euler (transform.eulerAngles + bulletEulerAngles));
+
+		}
+	}
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour {
+	public static UIManager _instance;
+
+
+	public Text levelNum;
+	public Text attackPowerNum;
+
+
+	public int levelN;
+	public int scoreN;
+	public int attackN;
+	public int attackPower;
+
+	private void Awake(){
+		_instance = this;
+
+	}
+	public void AddScore(){
+		scoreN += 1;
+	}
+	private void AddLevel(){
+		if (scoreN < 1) {
+			levelN = 1;
+			attackPower = 1;
+			attackN = 1;
+		}
+		else if (scoreN >= 1 && scoreN <3) {
+			levelN = 2;
+			attackPower = 3;
+			attackN = 3;
+		} 
+		else if (scoreN >= 3 && scoreN < 5) {
+			levelN = 3;
+			attackPower = 5;
+			attackN = 5;
+
+		}
+		else if(scoreN >= 5){
+			levelN = 4;
+			attackPower = 7;
+			attackN = 7;
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AddLevel ();
+		attackPowerNum.text = attackN.ToString ();
+		levelNum.text = levelN.ToString ();
+	}
+}
