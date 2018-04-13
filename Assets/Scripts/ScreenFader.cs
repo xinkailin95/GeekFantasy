2b00eea138dbefb519b4fe6a5dcf4c644750fe89
xinkai <xinkailin1995@gmail.com>
2018-04-13 04:57:47 -0400

    change building and add fading

---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenFader : MonoBehaviour
+{
+
+	Animator anim;
+	bool isFading = false;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	public IEnumerator FadeToClear ()
+	{
+		isFading = true;
+		anim.SetTrigger ("FadeIn");
+
+		while (isFading)
+			yield return null;
+	}
+
+	public IEnumerator FadeToBlack ()
+	{
+		isFading = true;
+		anim.SetTrigger ("FadeOut");
+
+		while (isFading)
+			yield return null;
+	}
+
+	// Update is called once per frame
+	void AnimationComplete ()
+	{
+		isFading = false;
+	}
+}
