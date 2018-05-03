7e175bcefbf5179195a764a0b8bc9c20c5488169
xinkai <xinkailin1995@gmail.com>
2018-05-02 20:49:42 -0400

    add new enemy

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Boss : MonoBehaviour
+{
+	public static Boss _instance;
+	public float speed;
+	public float backUptime = 0.1f;
+
+	public int attackPower = 1;
+
+	public Transform hero;
+	private Vector3 initPosition;
+	private GameObject player;
+	private float distance;
+	private Vector3 prevPosition;
+	private Vector3 newPosition;
+	private Rigidbody2D r2d;
+	private Vector3 rPosition;
+	private Vector3 currentDest;
+	private Vector2 destination;
+	private Animator anim;
+	bool loop;
+	public GameObject enemyBulletPre;
+	private float timeVal;
+	public GameObject healthBar;
+
+
+	//public int enemyLife = 3;
+
+	void Awake ()
+	{
+		anim = GetComponent<Animator> ();
+		initPosition = transform.position;
+		r2d = GetComponent<Rigidbody2D> ();
+		_instance = this;
+
+		Instantiate (healthBar, this.transform);
+
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+		hero = player.transform;
+
+		float x = Random.Range (-10, 10);
+		float y = Random.Range (-10, 10);
+		rPosition = new Vector3 (initPosition.x + x, initPosition.y + y, initPosition.z);
+		currentDest = rPosition;
+		loop = true;
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Debug.DrawLine (hero.position, transform.position, Color.red);
+
+		//newPosition = Vector3.zero;
+		distance = Vector3.Distance (hero.position, transform.position);
+		if (distance < 15 && Player._instance.curplayerLife > 0) {
+			// move toward player
+			newPosition = Vector3.MoveTowards (transform.position, hero.position, speed * Time.deltaTime);
+			r2d.MovePosition (newPosition);
+
+		} else if (loop == true) {
+			// idling
+			newPosition = Vector3.MoveTowards (transform.position, currentDest, speed * Time.deltaTime);
+			r2d.MovePosition (newPosition);
+			backUptime += 0.1f;
+			// Debug.Log (backUptime);
+			if (Vector3.Equals (transform.position, newPosition) || backUptime > 15f) {
+				backUptime = 0.1f;
+				loop = false;
+			}
+		} else {
+			// back to original position
+			newPosition = Vector3.MoveTowards (transform.position, initPosition, speed * Time.deltaTime);
+			r2d.MovePosition (newPosition);
+			if (Vector3.Equals (transform.position, newPosition)) {
+				loop = true;
+			}
+		}
+
+		Vector3 sub = newPosition - transform.position;
+		anim.SetFloat ("input_x", (sub.x < 0 ? -1f : 1f));
+		anim.SetFloat ("input_y", (sub.y < 0 ? -1f : 1f));
+
+		if (timeVal >= 2f) {
+
+			EnemyAttack (distance);
+			timeVal = 0;
+
+		} else {
+
+			timeVal += Time.deltaTime;
+
+		}
+	}
+
+
+	private void EnemyAttack (float a)
+	{
+		if (a < 10 && Player._instance.curplayerLife > 0) {
+			Instantiate (enemyBulletPre, transform.position, Quaternion.Euler (transform.eulerAngles + new Vector3 (0, 0, 30)));
+			Instantiate (enemyBulletPre, transform.position, Quaternion.Euler (transform.eulerAngles + new Vector3 (0, 0, 60)));
+			Instantiate (enemyBulletPre, transform.position, Quaternion.Euler (transform.eulerAngles));
+			Instantiate (enemyBulletPre, transform.position, Quaternion.Euler (transform.eulerAngles));
+		}
+	}
+}
--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -2,38 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyManager : MonoBehaviour {
+public class EnemyManager : MonoBehaviour
+{
 	public GameObject[] enemyGOs;
-	public int[] enemyLife = new int[16];
-	public bool[] isDead = new bool[16];
-	public bool[] hasKey = new bool[6]; 
-	private float distance; 
+	public int[] enemyLife = new int[20];
+	public bool[] isDead = new bool[20];
+	public bool[] hasKey;
+	private float distance;
 	public GameObject keyPrefab;
 
-	public static EnemyManager _instance;	
+	public static EnemyManager _instance;
 	// Use this for initialization
-	private void Awake(){
+	private void Awake ()
+	{
 		_instance = this;
-        for (int i = 1; i < enemyGOs.Length; i++)
-        {
-            enemyLife[i] = 10;
-            isDead[i] = false;
+		for (int i = 1; i < enemyGOs.Length; i++) {
+			enemyLife [i] = 10;
+			isDead [i] = false;
 
 
-        }
+		}
 		for (int i = 0; i < hasKey.Length; i++) {
 			hasKey [i] = false;
 		}
-        enemyLife[0] = 84;
+		enemyLife [0] = 84;
 
-    }
-	void Start () {
+	}
+
+	void Start ()
+	{
 		
 
 	}
 
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		for (int i = 1; i < enemyGOs.Length; i++) {
 			if (enemyLife [i] < 0 && isDead [i] == false) {
 				UIManager._instance.AddScore ();
@@ -46,27 +50,27 @@ public class EnemyManager : MonoBehaviour {
 		}
 
 		for (int i = 1; i < enemyGOs.Length; i++) {
-			if (enemyLife [1] < 0 && enemyLife [2] < 0 && enemyLife [3] < 0 && hasKey[0] == false) {
+			if (enemyLife [1] < 0 && enemyLife [2] < 0 && enemyLife [3] < 0 && enemyLife [16] < 0 && hasKey [0] == false) {
 				Instantiate (keyPrefab, transform.position, Quaternion.Euler (transform.eulerAngles));
 				UIManager._instance.addKey ();
 				hasKey [0] = true;
-			} else if (enemyLife [4] < 0 && enemyLife [5] < 0 && enemyLife [6] < 0 && hasKey[1] == false ) {
+			} else if (enemyLife [4] < 0 && enemyLife [5] < 0 && enemyLife [6] < 0 && hasKey [1] == false) {
 				Instantiate (keyPrefab, transform.position, Quaternion.Euler (transform.eulerAngles));
 				UIManager._instance.addKey ();
 				hasKey [1] = true;
-			} else if (enemyLife [7] < 0 && enemyLife [8] < 0 && enemyLife [9] < 0 && enemyLife [10] < 0 && hasKey[2] == false) {
+			} else if (enemyLife [7] < 0 && enemyLife [8] < 0 && enemyLife [9] < 0 && enemyLife [10] < 0 && hasKey [2] == false) {
 				Instantiate (keyPrefab, transform.position, Quaternion.Euler (transform.eulerAngles));
 				UIManager._instance.addKey ();
 				hasKey [2] = true;
-			} else if (enemyLife [11] < 0 && enemyLife [12] < 0 && hasKey[3] == false ) {
+			} else if (enemyLife [11] < 0 && enemyLife [12] < 0 && hasKey [3] == false) {
 				Instantiate (keyPrefab, transform.position, Quaternion.Euler (transform.eulerAngles));
 				UIManager._instance.addKey ();
 				hasKey [3] = true;
-			} else if (enemyLife [13] < 0 && enemyLife [14] < 0 && hasKey[4] == false ) {
+			} else if (enemyLife [13] < 0 && enemyLife [14] < 0 && hasKey [4] == false) {
 				Instantiate (keyPrefab, transform.position, Quaternion.Euler (transform.eulerAngles));
 				UIManager._instance.addKey ();
 				hasKey [4] = true;
-			} else if (enemyLife [15] < 0 && hasKey[5] == false) {
+			} else if (enemyLife [15] < 0 && enemyLife [18] < 0 && enemyLife [17] < 0 && hasKey [5] == false) {
 				Instantiate (keyPrefab, transform.position, Quaternion.Euler (transform.eulerAngles));
 				UIManager._instance.addKey ();
 				hasKey [5] = true;
